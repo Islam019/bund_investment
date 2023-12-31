6cf4c594f73f3f96668dffa4ddfc9f821b2fd3c1
Eslam Osama <islamusama56@gmail.com>
2023-12-31 23:06:20 +0200

    refactor: Create isolate widget for features

--- lib/features/home/presentation/pages/home_screen.dart
+++ lib/features/home/presentation/pages/home_screen.dart
@@ -1,6 +1,4 @@
-import 'package:bund/core/themes/text_styles.dart';
-import 'package:bund/features/home/presentation/widgets/condition_card.dart';
-import 'package:bund/features/home/presentation/widgets/feature_card.dart';
+import 'package:bund/features/home/presentation/widgets/features_section.dart';
 import 'package:bund/features/home/presentation/widgets/home_appbar.dart';
 import 'package:bund/features/home/presentation/widgets/home_slider.dart';
 import 'package:flutter/material.dart';
@@ -11,77 +9,15 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
+    return const SingleChildScrollView(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          const HomeAppBar(),
-          const Gap(16),
-          const HomeSlider(),
-          const Gap(14),
-          Padding(
-            padding: const EdgeInsets.only(left: 16),
-            child: Text(
-              'Conditions',
-              style: TextStyles.subTitleText,
-            ),
-          ),
-          const Gap(16),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                ConditionCard(icon: 'deposit', title: 'No Minimum Deposit'),
-                ConditionCard(
-                    icon: 'bank', title: '\$15/Month (Paid Annually)'),
-              ],
-            ),
-          ),
-          const Gap(14),
-          Padding(
-            padding: const EdgeInsets.only(left: 16),
-            child: Text(
-              'What You Get',
-              style: TextStyles.subTitleText,
-            ),
-          ),
-          const Gap(16),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    FeatureCard(icon: 'bank', title: 'Swiss Bank Account'),
-                    FeatureCard(icon: 'card', title: 'Mastercard Prepaid'),
-                    FeatureCard(
-                        icon: 'thunder', title: 'Account Open Same Day'),
-                  ],
-                ),
-                const Gap(16),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    FeatureCard(
-                        icon: 'umbrella', title: 'Protected up to \$100,000'),
-                    FeatureCard(
-                      icon: 'green',
-                      title: 'Investments Portfolios',
-                      isActive: false,
-                    ),
-                    FeatureCard(
-                      icon: 'locker',
-                      title: 'Deposits Options',
-                      isActive: false,
-                    ),
-                  ],
-                ),
-                const Gap(8), // handle the Final Gap
-              ],
-            ),
-          ),
+          HomeAppBar(),
+          Gap(16),
+          HomeSlider(),
+          Gap(14),
+          FeaturesSection(),
         ],
       ),
     );
